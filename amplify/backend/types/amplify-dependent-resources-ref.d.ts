d2ed6980b8162bba4c58543f9d5762c799e1b20e
田原崇 <taharatakashi@taharatakashinoMacBook-Air-2.local>
2024-04-21 15:47:50 +0900

    Update until setting Direct Lambda Resolver and connecting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "gameonsvelteandflow": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
